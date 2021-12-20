7434cb2120f0d817f18b38c75a2ce6c75606b218
shouravrahman <shouravrahman006@gmail.com>
2021-12-20 20:27:30 +0600

    ssr fixed with emotion cache

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,23 @@
 import { useEffect } from 'react'
 import '../styles/globals.css'
+import { CacheProvider } from '@emotion/react'
+import createEmotionCache from '../utils/createEmotionCache'
 
-function MyApp({ Component, pageProps }) {
-	useEffect(() => {
-		const jssStyles = document.querySelector('#jss-server-side')
-		if (jssStyles) {
-			jssStyles.parentElement.removeChild(jssStyles)
-		}
-	}, [])
-	return <Component {...pageProps} />
+const clientSideEmotionCache = createEmotionCache()
+function MyApp(props) {
+	const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
+
+	// useEffect(() => {
+	// 	const jssStyles = document.querySelector('#jss-server-side')
+	// 	if (jssStyles) {
+	// 		jssStyles.parentElement.removeChild(jssStyles)
+	// 	}
+	// }, [])
+	return (
+		<CacheProvider value={emotionCache}>
+			<Component {...pageProps} />
+		</CacheProvider>
+	)
 }
 
 export default MyApp
